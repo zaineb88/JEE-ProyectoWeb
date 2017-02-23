8602377599f2fc22819b5716f0e0d2a51eb1e9ea
zaineb88 <z.m.malak@hotmail.com>
2017-02-23 17:46:04 +0100

    ejemlo log

--- proyectoweb1/src/CuentaPagenas.java
+++ proyectoweb1/src/CuentaPagenas.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 /**
  * Servlet implementation class CuentaPagenas
  */
@@ -16,6 +18,7 @@ public class CuentaPagenas extends HttpServlet {
 	private int contador;
 
 	private static final long serialVersionUID = 1L;
+	private final static Logger log = Logger.getLogger("mylog");
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -29,14 +32,18 @@ public class CuentaPagenas extends HttpServlet {
 		// TODO Auto-generated method stub
 		super.init();
 		contador=0;
-	}
+		
+	}//log.info("El contador se inicia " + contador);
+		
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	//	response.getWriter().append("Served at: ").append(request.getContextPath());
+	
 		response.getWriter().append("Numero de visitas : "+contador);
+		log.info("Contador cuenta bien  " );
 	}
 
 	/**
