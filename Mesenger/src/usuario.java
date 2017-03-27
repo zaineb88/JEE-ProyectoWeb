487abfd5c0bbd702104a4ae8a74d0d1b857f62aa
zaineb88 <z.m.malak@hotmail.com>
2017-03-27 20:13:33 +0200

    app enviar mesajes con android

---
+++ Mesenger/src/usuario.java
@@ -0,0 +1,39 @@
+
+public class usuario {
+	
+	private int id;
+	private String nombre;
+	private String mensajeTX;
+	private String mensajeRX;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getMensajeTX() {
+		return mensajeTX;
+	}
+	public void setMensajeTX(String mensajeTX) {
+		this.mensajeTX = mensajeTX;
+	}
+	public String getMensajeRX() {
+		return mensajeRX;
+	}
+	public void setMensajeRX(String mensajeRX) {
+		this.mensajeRX = mensajeRX;
+	}
+
+	
+	
+	
+	
+}
